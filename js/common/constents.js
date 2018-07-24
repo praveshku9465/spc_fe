6080792b0134e2945adeed3c33e6c91cabcbede0
Pravesh Kumar <pravesh@atlogys.com>
2018-07-24 12:00:17 +0530

    first commit

---
+++ js/common/constents.js
@@ -0,0 +1,10 @@
+export default {
+  STORE_KEY: 'a56z0fzrNpl^2',
+  BASE_URL: 'http://127.0.0.1:3000/v1/',
+  COLOR: {
+    ORANGE: '#C50',
+    DARKBLUE: '#0F3274',
+    LIGHTBLUE: '#6EA8DA',
+    DARKGRAY: '#999',
+  },
+};
